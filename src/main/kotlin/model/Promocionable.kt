f5d81a0a2103368bc0879f803535b65870acdf4d
LeoOlivaresD <cspraword@gmail.com>
2025-11-10 22:31:22 -0300

    creacion de Promocionable

---
+++ src/main/kotlin/model/Promocionable.kt
@@ -0,0 +1,8 @@
+package model
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Promocionable(
+    val descuento: Double,
+    val descripcion: String = "Producto en promoción"
+)
